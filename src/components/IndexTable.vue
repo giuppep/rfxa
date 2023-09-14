df48436de424f982d34062d154fb67a553a1750e
Giuseppe Papallo <code@giuppep.dev>
2023-09-14 18:19:11 -0300

    move table to separate component

---
+++ src/components/IndexTable.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { IndexValue } from "@/models/finance"
+
+defineProps<{ indexValues: IndexValue[] }>()
+</script>
+
+<template>
+    <table>
+        <thead>
+            <th>Date</th>
+            <th>Value</th>
+        </thead>
+        <tbody>
+            <tr v-for="entry in indexValues">
+                <td class="capitalize">
+                    {{
+                        Intl.DateTimeFormat("en-GB", {
+                            month: "long",
+                        }).format(entry.date)
+                    }}
+                </td>
+                <td class="slashed-zero">
+                    {{ (100 * entry.value).toFixed(2) }}%
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
