401469fd4013691839bb81c0517b74b86179853a
Giuseppe Papallo <code@giuppep.dev>
2026-06-10 15:18:28 -0300

    setup views and router

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const navItems = [{ label: "Indices", to: "/indices" }]
+</script>
+
+<template>
+    <nav
+        class="flex flex-wrap items-center justify-between gap-4 px-4 py-3 sm:px-6"
+    >
+        <router-link to="/" class="text-lg font-bold">rfxa</router-link>
+        <ul class="flex gap-4">
+            <li v-for="item in navItems" :key="item.label">
+                <router-link :to="item.to">{{ item.label }}</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
