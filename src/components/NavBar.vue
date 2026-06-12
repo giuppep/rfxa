12e4b6be02043b78bc1006b0324ea22d123f9593
Giuseppe Papallo <code@giuppep.dev>
2026-06-12 14:07:43 -0300

    layout and styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
-const navItems = [{ label: "Indices", to: "/indices" }]
+import { useRoute } from "vue-router"
+
+const route = useRoute()
+const navItems = [{ label: "Indices", to: "/indices", name: "indices" }]
 </script>
 
 <template>
-    <nav class="flex flex-wrap items-center justify-between gap-4 px-4 sm:px-6">
-        <router-link to="/" class="text-lg font-bold">rfxa</router-link>
+    <nav
+        class="flex flex-wrap items-center justify-start bg-slate-50 gap-4 px-4 sm:px-6 py-2"
+    >
+        <RouterLink to="/" class="text-lg font-bold">rfxa</RouterLink>
         <ul class="flex gap-4">
             <li v-for="item in navItems" :key="item.label">
-                <router-link :to="item.to">{{ item.label }}</router-link>
+                <RouterLink
+                    :to="item.to"
+                    class="text-slate-500 hover:text-slate-900"
+                    active-class="text-orange-500"
+                >
+                    {{ item.label }}
+                </RouterLink>
             </li>
         </ul>
     </nav>
