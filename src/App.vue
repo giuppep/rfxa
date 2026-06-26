eb486b3891ab9b58487d301dd3f035151f967160
Giuseppe Papallo <code@giuppep.dev>
2026-06-26 11:09:38 -0300

    improve main layout + add footer with link to website

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,24 @@ import NavBar from "./components/NavBar.vue"
 </script>
 
 <template>
-    <NavBar class="" />
-    <div class="overflow-x-clip">
-        <router-view class="px-4 md:px-8 py-2" />
+    <div class="grid min-h-dvh grid-rows-[auto_1fr_auto]">
+        <NavBar />
+
+        <main class="px-4 py-2 md:px-8">
+            <router-view />
+        </main>
+
+        <footer
+            class="flex justify-end bg-olive-100 px-4 py-1 text-xs font-medium text-olive-500"
+        >
+            <span>
+                © 2026
+                <a
+                    href="https://giuppep.dev"
+                    class="font-semibold hover:text-olive-800"
+                    >giuppep.dev</a
+                >
+            </span>
+        </footer>
     </div>
 </template>
